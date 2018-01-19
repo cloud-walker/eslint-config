2210edb70db30512470536cd34b42110d6972682
Luca Barone <baro.luc@gmail.com>
2018-01-19 18:09:29 +0100

    adds react/jsx-uses-react and react/jsx-uses-vars to react config

--- react.js
+++ react.js
@@ -4,5 +4,7 @@ module.exports = {
     'react/jsx-no-comment-textnodes': 'warn',
     'react/jsx-no-duplicate-props': ['warn', {ignoreCase: true}],
     'react/jsx-no-target-blank': 'warn',
+    'react/jsx-uses-react': 'error',
+    'react/jsx-uses-vars': 'error',
   },
 }
